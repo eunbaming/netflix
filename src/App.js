6010c0e03ff437a9ed2b1739dbd2d523e90bcf6e
kimyuna <dbskqnsghd@naver.com>
2023-02-17 16:28:34 +0900

    render  navigation

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Movies from './pages/Movies';
 import MovieDetail from './pages/MovieDetail';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
     <div>
+      <Navigation />
       <Routes>
         <Route path='/' element={<Home />}></Route>
         <Route path='/movies' element={<Movies />}></Route>
