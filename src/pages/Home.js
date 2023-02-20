3f5cf3e39f4c5983e430cd6786d2d63bd2866a97
kimyuna <dbskqnsghd@naver.com>
2023-02-20 17:15:43 +0900

    render banner

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,18 @@
 import React, { useEffect } from 'react'
 import { movieAction } from '../redux/actions/movieAction'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import Banner from '../components/Banner'
 
 const Home = () => {
   const dispatch = useDispatch()
+  const {popularMovies, topRatedMovies, upComingMovies} = useSelector((state) => state.movie)
+  
   useEffect(() => {
     dispatch(movieAction.getMovies())
   }, [])
   return (
     <div>
-      Home
+      {popularMovies.results && <Banner movie={popularMovies.results[0]}/>}
     </div>
   )
 }
