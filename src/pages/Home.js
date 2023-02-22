ff9ba7a9fed74b472c85d051481274bede6e64a5
kimyuna <dbskqnsghd@naver.com>
2023-02-22 09:36:47 +0900

    loading-spinner,movie card

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,17 +2,29 @@ import React, { useEffect } from 'react'
 import { movieAction } from '../redux/actions/movieAction'
 import { useDispatch, useSelector } from 'react-redux'
 import Banner from '../components/Banner'
+import MovieSlide from '../components/MovieSlide'
+import ClipLoader from "react-spinners/ClipLoader";
 
 const Home = () => {
   const dispatch = useDispatch()
-  const {popularMovies, topRatedMovies, upComingMovies} = useSelector((state) => state.movie)
+  const {popularMovies, topRatedMovies, upComingMovies, loading} = useSelector((state) => state.movie)
   
   useEffect(() => {
     dispatch(movieAction.getMovies())
   }, [])
+
+  if (loading) {
+    return <div className='loading'><ClipLoader color="fff" loading={loading} size={150} /></div>
+  }
   return (
     <div>
       {popularMovies.results && <Banner movie={popularMovies.results[0]}/>}
+      <h1>Popular Movie</h1>
+      <MovieSlide movie={popularMovies}/>
+      <h1>Top rated Movie</h1>
+      <MovieSlide movie={topRatedMovies}/>
+      <h1>upComing Movie</h1>
+      <MovieSlide movie={upComingMovies}/>
     </div>
   )
 }
--- src/redux/actions/movieAction.js
+++ src/redux/actions/movieAction.js
@@ -3,21 +3,29 @@ import api from "../api"
 function getMovies () {
   const API_KEY = process.env.REACT_APP_API_KEY
   return async (dispatch) => {
-    const popularMovieApi = api.get(`/movie/popular?api_key=${API_KEY}&page=1`) // .http 함수
-    const topRatedApi = api.get(`/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`)
-    const upComingApi = api.get(`/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`)
+    try {
+      dispatch({type : "GET_MOVIES_REQUEST"})
+      const popularMovieApi = api.get(`/movie/popular?api_key=${API_KEY}&page=1`) // .http 함수
+      const topRatedApi = api.get(`/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`)
+      const upComingApi = api.get(`/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`)
 
-    let [popularMovies, topRatedMovies, upComingMovies] = await Promise.all([popularMovieApi, topRatedApi, upComingApi])
-    console.log(popularMovies, topRatedMovies, upComingMovies)
+      let [popularMovies, topRatedMovies, upComingMovies] = await Promise.all([popularMovieApi, topRatedApi, upComingApi])
+      console.log(popularMovies, topRatedMovies, upComingMovies)
 
-    dispatch({
-      type : "GET_MOVIES_SUCCESS",
-      payload : {
-        popularMovies : popularMovies.data,
-        topRatedMovies : topRatedMovies.data,
-        upComingMovies : upComingMovies.data
-      }
-    })
+      dispatch({
+        type : "GET_MOVIES_SUCCESS",
+        payload : {
+          popularMovies : popularMovies.data,
+          topRatedMovies : topRatedMovies.data,
+          upComingMovies : upComingMovies.data
+        }
+      })
+    } catch (error) {
+      dispatch({
+        type : "GET_MOVIES_FAILURE"
+      })
+    }
+
   }
 }
 
